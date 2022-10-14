be245f4f0eec172434d93dbd88a26dd0af4fd650
hibo2261 <hborse1@gmail.com>
2022-10-15 01:08:08 +0530

    [Himanshu] : Created an AddressBook

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.addressbook;
+
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        /**
+         * Creating object of class Contact and calling the method
+         */
+        System.out.println("========================================");
+        System.out.println("***** Welcome to Address Book System *****");
+        System.out.println("========================================");
+        Contact C = new Contact();
+        C.contacts();
+    }
+}
